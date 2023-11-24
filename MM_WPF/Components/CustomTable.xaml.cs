a16e8e8a46ff616bc663f04420c8f0360cb432c8
TTLC198 <yorehacked@gmail.com>
2023-11-24 16:20:44 +0300

    Changes

---
+++ MM_WPF/Components/CustomTable.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace MM_WPF.Components;
+
+public partial class CustomTable : UserControl, INotifyPropertyChanged
+{
+    public ObservableCollection<TableItem> Items
+    {
+        get => (ObservableCollection<TableItem>) GetValue(ItemsProperty);
+        set => SetValue(ItemsProperty, value);
+    }
+    
+    public static readonly DependencyProperty ItemsProperty =
+        DependencyProperty.Register(
+            nameof(Items),
+            typeof(ObservableCollection<TableItem>),
+            typeof(CustomTable),
+            new PropertyMetadata(new ObservableCollection<TableItem>()));
+    
+    public static readonly DependencyProperty ColumnsProperty =
+        DependencyProperty.Register(
+            nameof(Columns),
+            typeof(int),
+            typeof(CustomTable),
+            new PropertyMetadata(default(int)));
+
+    public int Columns
+    {
+        get => (int)GetValue(ColumnsProperty);
+        set => SetValue(ColumnsProperty, value);
+    }
+
+    public static readonly DependencyProperty RowsProperty =
+        DependencyProperty.Register(
+            nameof(Rows),
+            typeof(int),
+            typeof(CustomTable),
+            new PropertyMetadata(default(int)));
+
+    public int Rows
+    {
+        get => (int)GetValue(RowsProperty);
+        set => SetValue(RowsProperty, value);
+    }
+    
+    public static readonly DependencyProperty HeaderProperty =
+        DependencyProperty.Register(
+            nameof(Header),
+            typeof(string),
+            typeof(CustomTable),
+            new PropertyMetadata(default(string)));
+
+    public string Header
+    {
+        get => (string)GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+    
+    public CustomTable()
+    {
+        InitializeComponent();
+        DataContext = this;
+    }
+
+    private void CustomTable_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        GenerateColumns(Columns);
+        GenerateCells(Rows, Columns);
+    }
+
+    private void GenerateColumns(int count)
+    {
+        MainDataGrid.Columns.Clear();
+        for (var i = 0; i < count + 1; i++)
+        {
+            var headerBinding = new Binding("Header");
+            headerBinding.Mode = BindingMode.OneTime;
+            headerBinding.Path = new PropertyPath(Header);
+            
+            var itemBinding = new Binding($"{i}");
+            headerBinding.Mode = BindingMode.TwoWay;
+            headerBinding.Path = new PropertyPath(Items);
+            
+            MainDataGrid.Columns.Add(new DataGridTextColumn()
+            {
+                Header = i == 0
+                    ? Header
+                    : $"{i}",
+                Binding = i == 0
+                    ? headerBinding
+                    : itemBinding,
+                IsReadOnly = false
+            });
+        }
+    }
+
+    private void GenerateCells(int count, int columnsCount)
+    {
+        Items?.Clear();
+        for (var i = 0; i < count; i++)
+        {
+            var columns = new List<int>();
+            for (int j = 0; j < columnsCount - 1; j++)
+            {
+                columns.Add(0);
+            }
+
+            Items?.Add(new TableItem()
+            {
+                Header = $"{i + 1}",
+                Columns = columns
+            });
+        }
+    }
+
+    public void AddColumn(object sender, RoutedEventArgs e)
+    {
+        Columns += 1;
+        GenerateColumns(Columns);
+    }
+    
+    public void AddCell(object sender, RoutedEventArgs e)
+    {
+        Rows += 1;
+        GenerateCells(Rows, Columns);
+    }
+
+    public int[,] GetValues()
+    {
+        var columnsCount = MainDataGrid.Columns.Count;
+        var rowsCount = MainDataGrid.Items.Count;
+        var array = new int[columnsCount - 1, rowsCount - 1];
+
+        for (var i = 1; i < columnsCount; i++)
+        {
+            for (int j = 0; j < rowsCount; j++)
+            {
+                if (MainDataGrid.Items[j] is TableItem tableItem)
+                    array[i, j] = tableItem.Columns.ElementAtOrDefault(i);
+            }
+        }
+
+        return array;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
--- MM_WPF/MainWindow.xaml.cs
+++ MM_WPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,78 +13,45 @@ namespace MM_WPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        public ObservableCollection<TableItem> Items { get; set; } = new ObservableCollection<TableItem>();
+        public ObservableCollection<TableItem> Items
+        {
+            get => _items;
+            set
+            {
+                _items = Items;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<TableItem> _items = new ObservableCollection<TableItem>();
         
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = this;
         }
-
-        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
-        {
-            GenerateColumns(3);
-            GenerateCells(3, 3);
-            
-            MainDataGrid.Columns[0].Header = "Заводы/Магазины";
-        }
-
-        private void GenerateColumns(int count)
-        {
-            for (var i = 0; i < count + 1; i++)
-            {
-                MainDataGrid.Columns.Add(new DataGridTextColumn()
-                {
-                    Header = i,
-                    Binding = i == 0 
-                        ? new Binding("Header")
-                        : new Binding($"{i}"),
-                    IsReadOnly = false
-                });
-            }
-        }
         
-        private void GenerateCells(int count, int columnsCount)
-        {
-            for (var i = 0; i < count; i++)
-            {
-                var columns = new List<int>();
-                for (int j = 0; j < columnsCount - 1; j++)
-                {
-                    columns.Add(0);
-                }
-                Items.Add(new TableItem()
-                {
-                    Header = $"{i + 1}",
-                    Columns = columns
-                });
-            }
-        }
-
-        private int[,] GetValues()
-        {
-            var columnsCount = MainDataGrid.Columns.Count;
-            var rowsCount = MainDataGrid.Items.Count;
-            var array = new int[columnsCount - 1, rowsCount - 1];
-            
-            for (var i = 1; i < columnsCount; i++)
-            {
-                for (int j = 0; j < rowsCount; j++)
-                {
-                    if (MainDataGrid.Items[j] is TableItem tableItem)
-                        array[i, j] = tableItem.Columns.ElementAtOrDefault(i);
-                }
-            }
-
-            return array;
-        }
-
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            var temp = GetValues();
+            var temp = MainTable.GetValues();
             var t = temp;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
         }
     }
 }
